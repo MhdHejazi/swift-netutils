3fbb19262049e8b739dfe1b850a59f1e640f028d
MhdHejazi <mohammadhi@gmail.com>
2017-08-04 00:01:05 +0300

    Removed "import ifaddrs"

--- NetUtils/Interface.swift
+++ NetUtils/Interface.swift
@@ -1,7 +1,6 @@
 //  Copyright (c) 2015 Stefan van den Oord. All rights reserved.
 
 import Foundation
-import ifaddrs
 
 /**
  * This class represents a network interface in your system. For example, `en0` with a certain IP address.
